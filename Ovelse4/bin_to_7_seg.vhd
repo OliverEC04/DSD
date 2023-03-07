e77dd21d5ff0ecac817d0e897a2bd12051ee8234
IgorS01 <125971938+IgorS01@users.noreply.github.com>
2023-03-07 11:58:41 +0100

    seg tester

--- Ovelse4/bin_to_7_seg.vhd
+++ Ovelse4/bin_to_7_seg.vhd
@@ -16,15 +16,15 @@ ARCHITECTURE bin_to_7_seg_impl OF bin_to_7_seg IS
 BEGIN
 	with bin select sSeg <=
 	"1111111" when "0000",
-	"1001111" when "0001",
-	"1001111" when "0010",
-	"1001111" when "0011",
-	"1001111" when "0100",
-	"1001111" when "0101",
-	"1001111" when "0110",
-	"1001111" when "0111",
-	"1001111" when "1000",
-	"1001111" when "1001",
+	"1111110" when "0001",
+	"1111101" when "0010",
+	"1111011" when "0011",
+	"1110111" when "0100",
+	"1101111" when "0101",
+	"1011111" when "0110",
+	"0111111" when "0111",
+	"0000000" when "1000",
+	"0000100" when "1001",
 	"1111111" when others;
 
 END bin_to_7_seg_impl;
