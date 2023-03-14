13c48eb7424e730ca3fbc51d9cc74cfaf793ef39
OliverEC <oliverec04@gmail.com>
2023-03-14 23:10:52 +0100

    hej

---
+++ Ovelse4/hex_mux.vhd
@@ -0,0 +1,18 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.ALL;
+ENTITY hex_mux IS
+	PORT (
+		-- Input ports
+		
+
+		-- Output ports
+		
+	);
+END hex_mux;
+
+ARCHITECTURE hex_mux_impl OF hex_mux IS
+BEGIN
+	
+END hex_mux_impl;
