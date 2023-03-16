d6064b3bbe85ffdb0d4d7a9415d2e60e57b80fe2
IgorS01 <125971938+IgorS01@users.noreply.github.com>
2023-03-16 13:37:08 +0100

    mux some

--- Ovelse4/hex_mux.vhd
+++ Ovelse4/hex_mux.vhd
@@ -5,10 +5,14 @@ USE work.ALL;
 ENTITY hex_mux IS
 	PORT (
 		-- Input ports
-		
+		bin : IN STD_LOGIC_VECTOR (11 downto 0);
+		sel : IN STD_LOGIC_VECTOR (1 downto 0);
 
 		-- Output ports
-		
+		sSeg0 : OUT STD_LOGIC_VECTOR (6 downto 0);
+		sSeg1 : OUT STD_LOGIC_VECTOR (6 downto 0);
+		sSeg2 : OUT STD_LOGIC_VECTOR (6 downto 0);
+		sSeg3 : OUT STD_LOGIC_VECTOR (6 downto 0)
 	);
 END hex_mux;
 
---
+++ Ovelse4/hex_mux_tester.vhd.bak
@@ -0,0 +1,23 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.ALL;
+USE ieee.numeric_std.ALL;
+USE work.ALL;
+ENTITY hex_mux_tester IS
+	PORT (
+		-- Input ports
+		
+
+		-- Output ports
+		
+	);
+END hex_mux_tester;
+
+ARCHITECTURE hex_mux_tester_impl OF hex_mux_tester IS
+BEGIN
+dut: entity work.hex_mux
+	port map 
+	(
+		
+	);
+
+END hex_mux_tester_impl;
