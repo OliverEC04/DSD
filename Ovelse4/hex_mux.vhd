3fc3dd8465fd587fdb8c3a5ceaaaf0f6bc75eb2d
Oliver Engbjerg Christensen <oliverec04@gmail.com>
2023-03-17 13:26:24 +0100

    4 2

--- Ovelse4/hex_mux.vhd
+++ Ovelse4/hex_mux.vhd
@@ -11,12 +11,31 @@ ENTITY hex_mux IS
 		-- Output ports
 		sSeg0 : OUT STD_LOGIC_VECTOR (6 downto 0);
 		sSeg1 : OUT STD_LOGIC_VECTOR (6 downto 0);
-		sSeg2 : OUT STD_LOGIC_VECTOR (6 downto 0);
-		sSeg3 : OUT STD_LOGIC_VECTOR (6 downto 0)
+		sSeg2 : OUT STD_LOGIC_VECTOR (6 downto 0)
 	);
 END hex_mux;
 
 ARCHITECTURE hex_mux_impl OF hex_mux IS
-BEGIN
-	
+BEGIN	
+	process(sel)
+		begin
+			case sel is
+				when "01" =>
+					sSeg0 <= "0101111";
+					sSeg1 <= "0101111";
+					sSeg2 <= "0000110";
+				when "10" =>
+					sSeg0 <= "1111111";
+					sSeg1 <= "1111111";
+					sSeg2 <= "1111111";
+				when "11" =>
+					sSeg0 <= "0101011";
+					sSeg1 <= "1000000";
+					sSeg2 <= "1111111";
+				when others =>
+					sSeg0 <= "1111111";
+					sSeg1 <= "1111111";
+					sSeg2 <= "1111111";
+			end case;
+	end process;
 END hex_mux_impl;
