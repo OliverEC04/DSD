2decc711bf3d84e29edf894566d4a6c40a1bf783
OliverEC <oliverec04@gmail.com>
2023-03-17 23:46:50 +0100

    det godt nu

--- Ovelse4/hex_mux.vhd
+++ Ovelse4/hex_mux.vhd
@@ -9,52 +9,34 @@ ENTITY hex_mux IS
 		sel : IN STD_LOGIC_VECTOR (1 downto 0);
 
 		-- Output ports
-		--sSeg0 : OUT STD_LOGIC_VECTOR (6 downto 0);
-		--sSeg1 : OUT STD_LOGIC_VECTOR (6 downto 0);
-		--sSeg2 : OUT STD_LOGIC_VECTOR (6 downto 0)
 		tsseg : OUT STD_LOGIC_VECTOR (20 downto 0)
 	);
 END hex_mux;
 
 ARCHITECTURE hex_mux_impl OF hex_mux IS
-
-	--signal sSeg0_int : STD_LOGIC_VECTOR (6 downto 0);
-	--signal sSeg1_int : STD_LOGIC_VECTOR (6 downto 0);
-	--signal sSeg2_int : STD_LOGIC_VECTOR (6 downto 0);
-
-BEGIN
-	process(sel)
-		begin
-			case sel is
-			when "01" => -- Err
-				tsseg <= "010111101011110000110";
-			when "10" => -- bin2sev
-				tsseg <= "111111111111111111111";
-			when "11" => -- On
-				tsseg <= "010101110000001111111";
-			when others => -- Slukker naar andet
-				tsseg <= "111111111111111111111";
-		end case;
-	end process;
+	signal tissesek : STD_LOGIC_VECTOR (20 downto 0);
 	
+BEGIN
+	tsseg <= "111111111111111111111" when sel = "01" else
+				tissesek 					when sel = "10" else
+				"111111111111111111111" when sel = "11";
+					
 	dec0 : entity work.bin_to_7_seg
 		port map(
-			bin => bin(3 downto 0),
-			sSeg => tsseg(6 downto 0)
+			bin(3 downto 0) => bin(3 downto 0),
+			sSeg(6 downto 0) => tissesek(6 downto 0)
 		);
+		
 	dec1 : entity work.bin_to_7_seg
 		port map(
-			bin => bin(7 downto 4),
-			sSeg => tsseg(13 downto 7)
+			bin(3 downto 0) => bin(7 downto 4),
+			sSeg(6 downto 0) => tissesek(13 downto 7)
 		);
+		
 	dec2 : entity work.bin_to_7_seg
 		port map(
-			bin => bin(11 downto 8),
-			sSeg => tsseg(20 downto 14)
+			bin(3 downto 0) => bin(11 downto 8),
+			sSeg(6 downto 0) => tissesek(20 downto 14)
 		);
-	
-	--sSeg0 <= sSeg0_int(6 downto 0);
-	--sSeg1 <= sSeg1_int(6 downto 0);
-	--sSeg2 <= sSeg2_int(6 downto 0);
 	
 END hex_mux_impl;
