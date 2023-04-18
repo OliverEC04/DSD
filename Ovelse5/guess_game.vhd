7270dcfe362366dc9ce69a42c73b4a3bacc70d39
OliverEC <oliverec04@gmail.com>
2023-04-18 11:55:47 +0200

    det virker ikke

--- Ovelse5/guess_game.vhd
+++ Ovelse5/guess_game.vhd
@@ -21,26 +21,29 @@ architecture guess_game_beh of guess_game is
 begin
 	compare_logic : entity work.compare_logic port map (
 		input => inputs,
-      secret_value => secret_value std_logic_vector(7 downto 0)
+      secret_value => secret_value std_logic_vector(7 downto 0),
 		set => set
    );
 
 	mylatch : entity work.latch port map (
 		input => inputs,
-      secret_value => secret_value std_logic_vector(7 downto 0)
+      secret_value => secret_value std_logic_vector(7 downto 0),
 		set => set
    );
 	
 	mux1 : entity work.mux1 port map (
-		input => inputs,
-      secret_value => secret_value std_logic_vector(7 downto 0)
-		set => set
+		show => show,
+		secret_value => secret_value std_logic_vector(7 downto 0),
+		input => inputs std_logic_vector(7 downto 0),
+		bin => bin std_logic_vector (3 downto 0)
    );
 	
 	mux2 : entity work.mux2 port map (
-		input => inputs,
-      secret_value => secret_value std_logic_vector(7 downto 0)
-		set => set
+		result => result std_logic_vector(1 downto 0),
+		segment => segment std_logic_vector(6 downto 0),
+		hex1 => hex1 std_logic_vector (6 downto 0),
+		hex10 => hex2 std_logic_vector (6 downto 0),
+		try_btn => try_btn
    );
 	
 	hex1 : entity work.binToHex port map (
