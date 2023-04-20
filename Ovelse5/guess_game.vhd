59445ddbbba754d815798e7ca90a69882bb0cc9d
Morten-T <114079510+Morten-T@users.noreply.github.com>
2023-04-20 13:04:16 +0200

    lavede om paa guessGame

--- Ovelse5/guess_game.vhd
+++ Ovelse5/guess_game.vhd
@@ -10,49 +10,45 @@ show : in std_logic; -- Vis sat tal
 try : in std_logic; -- Sammenlign gaet
 
 -- Outputs
-hex1 : out std_logic_vector(6 downto 0); -- 7-segment enere
-hex10: out std_logic_vector(6 downto 0) -- 7-segment tiere
+hex1 : out std_logic_vector(6 downto 0); -- 7-segment en'ere
+hex10: out std_logic_vector(6 downto 0) -- 7-segment ti'ere
 );
 
 end;
 
 
 architecture guess_game_beh of guess_game is
-begin
-	compare_logic : entity work.compare_logic port map (
-		input => inputs,
-      secret_value => secret_value std_logic_vector(7 downto 0),
-		set => set
-   );
 
-	mylatch : entity work.latch port map (
-		input => inputs,
-      secret_value => secret_value std_logic_vector(7 downto 0),
-		set => set
-   );
-	
-	mux1 : entity work.mux1 port map (
-		show => show,
-		secret_value => secret_value std_logic_vector(7 downto 0),
-		input => inputs std_logic_vector(7 downto 0),
-		bin => bin std_logic_vector (3 downto 0)
-   );
-	
-	mux2 : entity work.mux2 port map (
-		result => result std_logic_vector(1 downto 0),
-		segment => segment std_logic_vector(6 downto 0),
-		hex1 => hex1 std_logic_vector (6 downto 0),
-		hex10 => hex2 std_logic_vector (6 downto 0),
-		try_btn => try_btn
-   );
-	
-	hex1 : entity work.binToHex port map (
-		bin => display_value(3 downto 0),
-      segment => bin2hex_out(6 downto 0)
-   );
+	signal compare_logic : std_logic_vector(1 downto 0);		-- true/false som viser om gættet er rigtigt
+	signal secret_value : std_logic_vector(7 downto 0);		-- værdi indtastet af spiller
+	signal display_value : std_logic_vector(7 downto 0);		-- 
+	signal binToHexOut : std_logic_vector(13 downto 8);		-- 
+
+begin
+
+		--Processen for at saette secret value fra input switches
+    process(set)
+    begin
+        if set = '0' then
+            secret_value <= inputs;
+        end if;
+    end process;
 	 
-	hex2 : entity work.binToHex port map (
-      bin => display_value(3 downto 0),
-      segment => bin2hex_out(6 downto 0)
-   );
+	     -- Processen der tjekker gaettet og opdaterer compare_logic signalet
+    process(try)
+    begin
+        if try = '0' then
+            -- Tjek om inputvalue er det samme som guess value
+            if inputs = secret_value then
+                compare_logic <= "01"; -- Correct guess
+            elsif secret_value > inputs then
+                compare_logic <= "10"; -- Input value is too low
+            elsif secret_value < inputs then
+                compare_logic <= "11"; -- Input value is too high
+            else
+                compare_logic <= "00"; -- No comparison made
+            end if;
+        end if;
+    end process;
+
 end guess_game_beh;
--- Ovelse5/guess_game_tester.vhd
+++ Ovelse5/guess_game_tester.vhd
@@ -5,7 +5,7 @@ entity guess_game_tester is
 port(
 -- Inputs
 inputs : in std_logic_vector(7 downto 0);
-set : in std_logic; -- Knap til at saette tal som staar paa 7seg
+KEY: std_logic_vector(2 downto 0); -- Knap til at saette tal som staar paa 7seg
 show : in std_logic; -- Vis sat tal
 try : in std_logic; -- Sammenlign gaet
 
@@ -20,13 +20,13 @@ end;
 architecture guess_game_tester_beh of guess_game_tester is
 begin
 	guess_game : entity work.guess_game port map (
-		inputs => inputs std_logic_vector(7 downto 0),
-		set => set std_logic, -- Knap til at saette tal som staar paa 7seg
-		show => show std_logic, -- Vis sat tal
-		try => try std_logic, -- Sammenlign gaet
+		inputs => inputs,
+		set => KEY(0), -- Knap til at sætte tal som står på 7-segment displayet
+		show => KEY(1), -- Vis sat tal
+		try => KEY(2), -- Sammenlign gæt
 
 		-- Outputs
-		hex1 => hex1 std_logic_vector(6 downto 0), -- 7-segment enere
-		hex10 => hex10 std_logic_vector(6 downto 0) -- 7-segment tiere
+		hex1 => hex1, -- 7-segment enere
+		hex10 => hex10 -- 7-segment tiere
    );
-end guess_game_tester_beh;
+end architecture guess_game_tester_beh;
