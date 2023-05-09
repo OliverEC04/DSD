76100da65a8d2a2785ca4a0fdc8bf799a53cbe2e
OliverEC <oliverec04@gmail.com>
2023-05-09 12:51:48 +0200

    ovelse8

---
+++ Ovelse8/my_gates.vhd
@@ -0,0 +1,22 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+use IEEE.NUMERIC_STD.ALL;
+
+entity my_gates is
+	port
+	( 
+		-- inputs
+		
+
+		-- outputs
+		
+	);
+end my_gates;
+
+architecture my_gates_impl of my_gates is
+
+	function my_xor
+
+begin
+
+end my_gates_impl;
