88785197f8e50e7be0305806ca95e56830370e11
OliverEC <oliverec04@gmail.com>
2023-05-09 13:33:47 +0200

    o8opg1 done

--- Ovelse8/my_gates_package.vhd
+++ Ovelse8/my_gates_package.vhd
@@ -2,21 +2,15 @@ library IEEE;
 use IEEE.STD_LOGIC_1164.ALL;
 use IEEE.NUMERIC_STD.ALL;
 
-entity my_gates is
-	port
-	( 
-		-- inputs
-		a: in std_logic;
-		b: in std_logic;
+package my_gates_package is
 
-		-- outputs
-		and_out: out std_logic;
-		or_out: out std_logic;
-		xor_out: out std_logic
-	);
-end my_gates;
+	function my_xor (a: std_logic; b: std_logic) return std_logic;
+	procedure my_and_or (a: in std_logic; b: in std_logic; signal and_out: out std_logic; signal or_out: out std_logic);
 
-architecture my_gates_impl of my_gates is
+end my_gates_package;
+
+package body my_gates_package is
+
 	-- XOR function
 	function my_xor (a: std_logic; b: std_logic) return std_logic is
 	begin
@@ -29,12 +23,5 @@ architecture my_gates_impl of my_gates is
 		and_out <= (a and b);
 		or_out <= (a or b);
 	end my_and_or;
-
-begin
-	-- Run XOR function
-	xor_out <= my_xor(a, b);
 	
-	-- Run AND OR procedure
-	my_and_or(a, b, and_out, or_out);
-	
-end my_gates_impl;
+end my_gates_package;
